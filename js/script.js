bb71e60c3492ee475865ea527f4c9badefa01f32
Inversionway <93317529+Inversionway@users.noreply.github.com>
2021-10-28 12:10:49 +0300

    Educate

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let hello = 'Привет Создатель!';
+let message = 'Зачем ты призвал меня?';
+
+alert (hello);
+alert (message);
+let one = 'Для постройки ';
+let two = 'Зиккурата ';
+let three = 'нужно больше золота ';
+alert (one+two+three);
+const MyBirthday = '25.10.1995 -';
+let BirthdayAlert = ' Дата Рождения Темного Властелина ';
+alert (MyBirthday+BirthdayAlert);
